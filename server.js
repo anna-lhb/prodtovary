2865408192623e7d15911254a8d0081ff13742d5
anna-lhb <hairguru.anna@gmail.com>
2026-06-29 14:24:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ import { readFile } from 'fs/promises';
 import pg from 'pg';
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://prodtovary.com');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
